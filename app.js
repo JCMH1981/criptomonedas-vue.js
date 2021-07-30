dd24e1ca46dab12eae2d7eade3faf97ea43a1d76
JCMH1981 <jcmhsoftware@gmail.com>
2021-07-30 06:12:14 +0000

    Crear bindings de datos bidireccionales en Vue.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = new Vue({
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: -10,
       color: 'f4f4f4',
+      value: 0,
       price: 8400,
       pricesWithDays: [
           { day: 'Lunes', value: 8400 },
@@ -25,6 +26,13 @@ const app = new Vue({
   computed: {
     title() {
       return `${this.name} - ${this.symbol}`;
+    },
+    
+    convertedValue() {
+      if (!this.value) {
+        return 0;
+      }
+      return this.value/this.price;
     }
   },
 
