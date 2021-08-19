409f38ff13591cf53b1ef2137d0fee0914d9f341
JCMH1981 <jcmhsoftware@gmail.com>
2021-08-19 22:46:28 +0000

    Crear rutas dinámicas

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from 'vue'
+import Router from 'vue-router'
 
-import Home from '@/views/Home';
-import Error from '@/views/Error';
-import About from '@/views/About';
+import Home from '@/views/Home'
+import Error from '@/views/Error'
+import About from '@/views/About'
+import CoinDetail from '@/views/CoinDetail'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   mode: 'history',
@@ -24,9 +25,15 @@ export default new Router({
     },
 
     {
+      path: '/coin/:id',
+      name: 'coin-detail',
+      component: CoinDetail
+    },
+
+    {
       path: '*',
       name: 'error',
       component: Error
     }
   ]
-});
+})
